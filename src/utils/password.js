8a943a485209b65d49a7dc20de17b3f7c563fde3
Ikrar06 <ikrargempurtrn@gmail.com>
2025-12-07 17:49:50 +0800

    remove duplicate password validation logic

--- src/utils/password.js
+++ src/utils/password.js
@@ -10,28 +10,7 @@ const comparePassword = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 };
 
-// Validasi kekuatan password
-const validatePasswordStrength = (password) => {
-  // Minimal 8 karakter
-  if (password.length < 8) {
-    return { valid: false, message: 'Password must be at least 8 characters' };
-  }
-
-  // Harus ada huruf besar
-  if (!/[A-Z]/.test(password)) {
-    return { valid: false, message: 'Password must contain at least one uppercase letter' };
-  }
-
-  // Harus ada angka
-  if (!/[0-9]/.test(password)) {
-    return { valid: false, message: 'Password must contain at least one number' };
-  }
-
-  return { valid: true };
-};
-
 module.exports = {
   hashPassword,
-  comparePassword,
-  validatePasswordStrength
+  comparePassword
 };
