bd164b3ed4a37c5a92d9970a992f6c3e5bccddc0
Ikrar06 <ikrargempurtrn@gmail.com>
2025-12-09 15:26:19 +0800

    feat: configure CORS with allowed origins and credentials

---
+++ src/middlewares/corsConfig.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+module.exports = cors(corsOptions);
