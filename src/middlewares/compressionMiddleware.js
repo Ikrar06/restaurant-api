93abd0433e914908b128ee190204c4e3862f1214
Ikrar06 <ikrargempurtrn@gmail.com>
2025-12-09 15:25:51 +0800

    feat: add response compression for responses > 1kb

---
+++ src/middlewares/compressionMiddleware.js
@@ -0,0 +1,13 @@
+const compression = require('compression');
+
+const compressionMiddleware = compression({
+  filter: (req, res) => {
+    if (req.headers['x-no-compression']) {
+      return false;
+    }
+    return compression.filter(req, res);
+  },
+  threshold: 1024
+});
+
+module.exports = compressionMiddleware;
